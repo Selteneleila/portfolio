588aa82d0ba3bd2dc1af106440854de0af5f5bf3
Leila Selten <layla_seltene@live.fr>
2024-07-30 12:18:59 +0300

    Fixed banner style

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button, Grid } from "@mui/material";
 import MySvg from "../../assets/logo.svg";
-import "./Banner.css";
+import "./banner.css";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 export default function Banner() {
   return (
-    <Grid className="container" container spacing={1}>
+    <Grid className="banner-container" container spacing={1}>
       <Grid item xs={7}>
         <div className="text-container">
           <h1>Hi, I'm Leila</h1>
@@ -26,7 +26,7 @@ export default function Banner() {
           </Button>
         </div>
       </Grid>
-      <Grid item xs={5}>
+      <Grid item xs={4}>
         <img src={MySvg} alt="Description of SVG" className="banner-img" />
       </Grid>
     </Grid>
