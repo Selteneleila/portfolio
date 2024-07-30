d744a7127077b3abd483cf4f66757357b87caa84
Leila Selten <layla_seltene@live.fr>
2024-07-30 12:10:56 +0300

    Navbar styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,9 @@
 import React from "react";
-import "./Navbar.css";
-import Logo from "../../assets/logo.png";
+import "./navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img className="logo-img" src={Logo} alt="logo" />
-      </div>
       <div className="navbar-items">
         <a href="#about">About Me</a>
         <a href="#skills">Skills</a>
